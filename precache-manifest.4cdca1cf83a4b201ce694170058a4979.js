5b0da51c12638911b69be23956156afc79e2fc22
rzegorz Stuczynski <stuczynski.g@gmail.com>
2023-03-09 14:12:35 +0100

    first commit2sdfdsdds

---
+++ precache-manifest.4cdca1cf83a4b201ce694170058a4979.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c399ee19c72146ed6d0de621f1384843",
+    "url": "/C:/repos/rpl/build/index.html"
+  },
+  {
+    "revision": "318a1990b0d34077fa21",
+    "url": "/C:/repos/rpl/build/static/css/main.c65623f0.chunk.css"
+  },
+  {
+    "revision": "05698ef62fec3976cfaf",
+    "url": "/C:/repos/rpl/build/static/js/2.df836e0f.chunk.js"
+  },
+  {
+    "revision": "318a1990b0d34077fa21",
+    "url": "/C:/repos/rpl/build/static/js/main.04a4ae28.chunk.js"
+  },
+  {
+    "revision": "137e5e2e94ac29581c4e",
+    "url": "/C:/repos/rpl/build/static/js/runtime~main.02e00331.js"
+  },
+  {
+    "revision": "c446158851c686c1d2f3231bf7015557",
+    "url": "/C:/repos/rpl/build/static/media/The-Witcher-3-Wild-Hunt-Sword-of-Destiny.c4461588.mp3"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "/C:/repos/rpl/build/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "/C:/repos/rpl/build/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "/C:/repos/rpl/build/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "/C:/repos/rpl/build/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "/C:/repos/rpl/build/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "/C:/repos/rpl/build/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "33211a17eb64aaa6b94254b72a70bcb9",
+    "url": "/C:/repos/rpl/build/static/media/smile.33211a17.svg"
+  },
+  {
+    "revision": "af24f45223adce75c1cb080faa9980eb",
+    "url": "/C:/repos/rpl/build/static/media/under-construction.af24f452.png"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "/C:/repos/rpl/build/static/media/view-info.415b027d.ico"
+  }
+]);
