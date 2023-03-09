40980ee046221caf55428bc296574dfab1ceeb69
rzegorz Stuczynski <stuczynski.g@gmail.com>
2023-03-09 14:25:48 +0100

    dsd

---
+++ precache-manifest.596a10a6edae2464b5972241a4a6759b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6ab59dd93861c50c606bf4f0c2d4b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5275fc50e05ec6a015e",
+    "url": "/static/css/main.c65623f0.chunk.css"
+  },
+  {
+    "revision": "b869c227efe873672d12",
+    "url": "/static/js/2.9608309a.chunk.js"
+  },
+  {
+    "revision": "e5275fc50e05ec6a015e",
+    "url": "/static/js/main.ab2d49ec.chunk.js"
+  },
+  {
+    "revision": "1f90f66380f498835703",
+    "url": "/static/js/runtime~main.dc087518.js"
+  },
+  {
+    "revision": "c446158851c686c1d2f3231bf7015557",
+    "url": "/static/media/The-Witcher-3-Wild-Hunt-Sword-of-Destiny.c4461588.mp3"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "33211a17eb64aaa6b94254b72a70bcb9",
+    "url": "/static/media/smile.33211a17.svg"
+  },
+  {
+    "revision": "af24f45223adce75c1cb080faa9980eb",
+    "url": "/static/media/under-construction.af24f452.png"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "/static/media/view-info.415b027d.ico"
+  }
+]);
