378a74ceee3312bc6f9a9caa426b319d1919c5e3
rzegorz Stuczynski <stuczynski.g@gmail.com>
2023-03-09 17:20:10 +0100

    dupa

---
+++ precache-manifest.3faa5816fdaceeb04fc803292abc043b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f0c3c4b78c4103459a66d0c818c068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3011d402f45a0034602c",
+    "url": "/static/css/main.c65623f0.chunk.css"
+  },
+  {
+    "revision": "bac39f0722fbf1b70fb4",
+    "url": "/static/js/2.786bd4ee.chunk.js"
+  },
+  {
+    "revision": "3011d402f45a0034602c",
+    "url": "/static/js/main.e54e8be9.chunk.js"
+  },
+  {
+    "revision": "1f90f66380f498835703",
+    "url": "/static/js/runtime~main.dc087518.js"
+  },
+  {
+    "revision": "e6da28c593a3ff8e02810c64e46aaa8f",
+    "url": "/static/media/Age-of-empires-2.e6da28c5.mp3"
+  },
+  {
+    "revision": "e064194e6c092da46e0109e329126547",
+    "url": "/static/media/Sevara-Nazarkhan-We-are-muay-thai.e064194e.mp3"
+  },
+  {
+    "revision": "c446158851c686c1d2f3231bf7015557",
+    "url": "/static/media/The-Witcher-3-Wild-Hunt-Sword-of-Destiny.c4461588.mp3"
+  },
+  {
+    "revision": "a48640a284521dc4184480c904702eab",
+    "url": "/static/media/all-programs.a48640a2.ico"
+  },
+  {
+    "revision": "199b5352639f3881890888b0df965beb",
+    "url": "/static/media/error.199b5352.wav"
+  },
+  {
+    "revision": "cfc00209c2e6a9df522c90e356be6241",
+    "url": "/static/media/find.cfc00209.svg"
+  },
+  {
+    "revision": "d2091307876be4c6bd5df626e3607ac5",
+    "url": "/static/media/humtopia.d2091307.png"
+  },
+  {
+    "revision": "e23232f0ee9089a33025c8a1aa81146b",
+    "url": "/static/media/narrator.e23232f0.ico"
+  },
+  {
+    "revision": "bfcfd28fbabd92589d0974b2760ed758",
+    "url": "/static/media/restart.bfcfd28f.ico"
+  },
+  {
+    "revision": "8e8ae78a182de1b6207be2f91244f3e8",
+    "url": "/static/media/restore.8e8ae78a.ico"
+  },
+  {
+    "revision": "33211a17eb64aaa6b94254b72a70bcb9",
+    "url": "/static/media/smile.33211a17.svg"
+  },
+  {
+    "revision": "2b1ac0490f1752e52c9fd9e3c474f207",
+    "url": "/static/media/this-wandering-day.2b1ac049.mp3"
+  },
+  {
+    "revision": "af24f45223adce75c1cb080faa9980eb",
+    "url": "/static/media/under-construction.af24f452.png"
+  },
+  {
+    "revision": "415b027d489dfadce00f9e229872e609",
+    "url": "/static/media/view-info.415b027d.ico"
+  }
+]);
